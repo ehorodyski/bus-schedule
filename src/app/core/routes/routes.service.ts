b830bd1590b0ca815d3961c0c4e3a6887bc7fcc3
Eric Horodyski <eric@horodyski.me>
2019-08-03 19:33:50 -0400

    Refactor VehicleLocationsService.refresh

--- src/app/core/routes/routes.service.ts
+++ src/app/core/routes/routes.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
-//import 'rxjs/add/operator/map';
 import { parseString } from 'xml2js';
 
 import { environment } from '../../../environments/environment';
--- src/app/core/vehicle-locations/vehicle-locations.service.ts
+++ src/app/core/vehicle-locations/vehicle-locations.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-//import 'rxjs/add/operator/map';
 import { Subject } from 'rxjs';
 import { parseString } from 'xml2js';
 
@@ -10,25 +9,27 @@ import { VehicleLoctationsResponse } from './vehicle-locations-response';
 @Injectable()
 export class VehicleLocationsService {
 
+  ///TODO: MOVE THIS TO NGRX
   data: Subject<VehicleLoctationsResponse>;
 
   constructor(private http: HttpClient) {
     this.data = new Subject();
   }
 
-  refresh(agency: string, since?: number): void {
-    // this.http.get(environment.dataServiceUrl, {
-    //   params: {
-    //     command: 'vehicleLocations',
-    //     a: agency,
-    //     t: since || 0
-    //   }
-    // }).map(res => res.text())
-    //   .subscribe(xml => this.unpackXML(xml));
+  refresh(agency: string, since: number = 0): void {
+    this.http.get(environment.dataServiceUrl, {
+      responseType: 'text',
+      params: {
+        command: 'vehicleLocations',
+        a: agency,
+        t: since.toString()
+      }
+    }).subscribe(xml => this.unpackXML(xml));
   }
 
   private unpackXML(xml: string) {
     parseString(xml, { explicitArray: false, mergeAttrs: true }, (err, result) => {
+      console.log('VL:', result);
       this.data.next({
         lastTime: parseInt(result.body.lastTime.time, 10),
         locations: !result.body.vehicle ? [] :
