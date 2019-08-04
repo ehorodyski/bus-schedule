293df79375770a5d560219a3e48b0a3ace4a03d4
Eric Horodyski <eric@horodyski.me>
2019-08-04 13:48:39 -0400

    Refactor App Component to remove references to services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
 import { select, Store } from '@ngrx/store';
 
-import { RouteOptionsService } from './core/route-options/route-options.service';
 import { Route } from './core/routes/route';
-import { RoutesService } from './core/routes/routes.service';
 import * as fromRoot from '../app/reducers';
 import { RoutesActions } from './core/actions';
 
@@ -13,33 +11,16 @@ import { RoutesActions } from './core/actions';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  private routesSubsciption: Subscription;
-
-  routes: Array<Route>;
+export class AppComponent implements OnInit {
 
   routes$: Observable<Array<Route>>;
 
-  constructor(
-    private routeOptions: RouteOptionsService,
-    private routesService: RoutesService,
-    private store: Store<fromRoot.State>
-  ) {
+  constructor(private store: Store<fromRoot.State>) {
     this.routes$ = store.pipe(select(fromRoot.getRoutesRoutes));
   }
 
   ngOnInit() {
-    this.routes$.subscribe(console.log);
-    // this.routesService.data.subscribe(r => {
-    //   console.log('RRRR', r);
-    //   this.routes = r;
-    //   // this.routes.forEach(route => this.routeOptions.showRoute('sf-muni', route.tag));
-    // });
-    // this.routesService.refresh('sf-muni');
     this.store.dispatch(RoutesActions.refresh({ agency: 'sf-muni' }));
   }
 
-  ngOnDestroy() {
-    this.routesSubsciption.unsubscribe();
-  }
 }
