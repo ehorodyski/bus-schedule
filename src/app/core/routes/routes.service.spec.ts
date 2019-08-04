1f655db60934783f3f84cd469d7fad0fef5b21bb
Eric Horodyski <eric@horodyski.me>
2019-08-04 16:19:28 -0400

    Refactor RouteService tests

--- src/app/core/routes/routes.service.spec.ts
+++ src/app/core/routes/routes.service.spec.ts
@@ -29,66 +29,66 @@ describe('RoutesService', () => {
       req.flush('<body></body>');
     });
 
-    //   it('emits new data', () => {
-    //     let body = '<body>';
-    //     body += route('1', '1-California');
-    //     body += route('1AX', '1AX-California A Express');
-    //     body += route('1BX', '1BX-California B Express');
-    //     body += route('2', '2-Clement');
-    //     body += route('3', '3-Jackson');
-    //     body += route('5', '5-Fulton');
-    //     body += route('5R', '5R-Fulton Rapid');
-    //     body += '</body>';
-    //     let connection: MockConnection;
-    //     mockBackend.connections.subscribe(c => connection = c);
-    //     let result: any;
-    //     service.data.subscribe(r => result = r);
-    //     service.refresh('sf-muni');
-    //     connection.mockRespond(new Response(new ResponseOptions({
-    //       status: 200,
-    //       body: body
-    //     })));
-    //     expect(result).toEqual([
-    //       { tag: '1', title: '1-California' },
-    //       { tag: '1AX', title: '1AX-California A Express' },
-    //       { tag: '1BX', title: '1BX-California B Express' },
-    //       { tag: '2', title: '2-Clement' },
-    //       { tag: '3', title: '3-Jackson' },
-    //       { tag: '5', title: '5-Fulton' },
-    //       { tag: '5R', title: '5R-Fulton Rapid' },
-    //     ]);
-    //   });
+    it('emits new data', () => {
+      let body = '<body>';
+      body += route('1', '1-California');
+      body += route('1AX', '1AX-California A Express');
+      body += route('1BX', '1BX-California B Express');
+      body += route('2', '2-Clement');
+      body += route('3', '3-Jackson');
+      body += route('5', '5-Fulton');
+      body += route('5R', '5R-Fulton Rapid');
+      body += '</body>';
 
-    //   it('emits an array of one with a single route in the response', () => {
-    //     let body = '<body>';
-    //     body += route('1BX', '1BX-California B Express');
-    //     body += '</body>';
-    //     let connection: MockConnection;
-    //     mockBackend.connections.subscribe(c => connection = c);
-    //     let result: any;
-    //     service.data.subscribe(r => result = r);
-    //     service.refresh('sf-muni');
-    //     connection.mockRespond(new Response(new ResponseOptions({
-    //       status: 200,
-    //       body: body
-    //     })));
-    //     expect(result).toEqual([{ tag: '1BX', title: '1BX-California B Express' }]);
-    //   });
+      let expectedResult = [
+        { tag: '1', title: '1-California' },
+        { tag: '1AX', title: '1AX-California A Express' },
+        { tag: '1BX', title: '1BX-California B Express' },
+        { tag: '2', title: '2-Clement' },
+        { tag: '3', title: '3-Jackson' },
+        { tag: '5', title: '5-Fulton' },
+        { tag: '5R', title: '5R-Fulton Rapid' },
+      ];
 
-    //   it('emits an empty array with no routes in the response', () => {
-    //     let body = '<body><lastTime time="1499622357839" />';
-    //     body += '</body>';
-    //     let connection: MockConnection;
-    //     mockBackend.connections.subscribe(c => connection = c);
-    //     let result: any;
-    //     service.data.subscribe(r => result = r);
-    //     service.refresh('sf-muni');
-    //     connection.mockRespond(new Response(new ResponseOptions({
-    //       status: 200,
-    //       body: body
-    //     })));
-    //     expect(result).toEqual([]);
-    //   });
+      const expectedUrl = 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=umn-twin';
+      service.refresh('umn-twin').subscribe(
+        (res) => expect(res).toEqual(expectedResult),
+        fail
+      );
+      const req = httpTestingController.expectOne(expectedUrl);
+      expect(req.request.method).toBe('GET');
+      req.flush(body);
+    });
+
+    it('emits an array of one with a single route in the response', () => {
+      let body = '<body>';
+      body += route('1BX', '1BX-California B Express');
+      body += '</body>';
+
+      let expectedResult = [{ tag: '1BX', title: '1BX-California B Express' }];
+
+      const expectedUrl = 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni';
+      service.refresh('sf-muni').subscribe(
+        (res) => expect(res).toEqual(expectedResult),
+        fail
+      );
+      const req = httpTestingController.expectOne(expectedUrl);
+      expect(req.request.method).toBe('GET');
+      req.flush(body);
+    });
+
+    it('emits an empty array with no routes in the response', () => {
+      let body = '<body><lastTime time="1499622357839" />';
+      body += '</body>';
+      const expectedUrl = 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni';
+      service.refresh('sf-muni').subscribe(
+        (res) => expect(res).toEqual([]),
+        fail
+      );
+      const req = httpTestingController.expectOne(expectedUrl);
+      expect(req.request.method).toBe('GET');
+      req.flush(body);
+    });
   });
 
   function route(tag: string, title: string) {
