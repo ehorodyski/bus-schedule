263043f8933b8ae2724c5cbfc2624d39448bf76d
Eric Horodyski <eric@horodyski.me>
2019-08-04 20:29:51 -0400

    Remove unused dependency

--- src/app/core/services/vehicle-locations.service.ts
+++ src/app/core/services/vehicle-locations.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Subject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
 import { parseString } from 'xml2js';
 
